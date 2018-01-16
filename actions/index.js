affed72464a205a1f4358b2d548af385a4d3e6b7
pmg1989 <972401854@qq.com>
2018-01-16 12:01:24 +0800

    fix reducer

--- actions/index.js
+++ actions/index.js
@@ -3,15 +3,15 @@ let nextTodoId = 2
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
   id: nextTodoId++,
-  text
+  text,
 })
 
 export const setVisibilityFilter = (filter) => ({
   type: 'SET_VISIBILITY_FILTER',
-  filter
+  filter,
 })
 
 export const toggleTodo = (id) => ({
   type: 'TOGGLE_TODO',
-  id
+  id,
 })
