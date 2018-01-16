00adacf62429ee80377609f3cbe273d6e17cf870
pmg1989 <972401854@qq.com>
2018-01-16 12:23:03 +0800

    fix bindActionCreators

--- actions/index.js
+++ actions/index.js
@@ -1,17 +1,1 @@
-let nextTodoId = 2
-
-export const addTodo = (text) => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text,
-})
-
-export const setVisibilityFilter = (filter) => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter,
-})
-
-export const toggleTodo = (id) => ({
-  type: 'TOGGLE_TODO',
-  id,
-})
+export * from './todos.js'
